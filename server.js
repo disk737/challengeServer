4c081c3d62f96f60e3bfdfd303e01ac2bf92fe3c
Juan Jose Muñoz Franco <Juan Jose Muoz Franco>
2017-05-13 19:50:31 -0500

    Primera Funcion

--- server.js
+++ server.js
@@ -1,12 +1,91 @@
+//////////////////////////////////////////////////////////////////////
+//																	//
+// Codigo Web Services de Memoria Colombia 							//
+// --------------------------------------------------				//
+//																	//
+// @author  Juan Jose Muñoz <disk737@hotmail.com>           		//
+// @version 0.0.2													//
+//																	//
+//////////////////////////////////////////////////////////////////////
+
+/* Módulos requeridos por la aplicación */
+
 var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+//////////////////////////////////////////////////////////////////
+/* Código a ejecutar al iniciar la conexión a la base de datos */
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+connection.connect(function(error){
+   if(error){
+   	console.log("Error conectandose a la base de datos");
+      throw error;
+   }else{
+      console.log('Conexion correcta.');
+   }
+});
+//////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////
+/* Configuración del analizador del cuerpo (request) y parámetros (response) */
+app.use(bodyParser.json()); // Body parser use JSON data
+app.use(bodyParser.urlencoded({ extended: false }));
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+// Definición de las rutas										//
+//////////////////////////////////////////////////////////////////
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+
+//////////////////////////////////////////////////////////////////
+//       API de Lider                                           //
+//////////////////////////////////////////////////////////////////
+
+/* Se consultan todos los retos */
+
+app.get('/challenge/leader/consultarReto', function(req, res, next) {
+
+	console.log('Consultando todos los retos ...');
+
+	// Establecer el tipo MIME de la respuesta
+	res.setHeader("Content-Type", "application/json");
+
+	// Consultar las entidades a la base de datos
+	var query = connection.query('SELECT * FROM challenge', 
+                                 function(err, rows) {
+
+		// Verificar si sucedió un error durante la consulta
+		if (err)
+		{
+			console.error(err);
+
+			res.status(500);	// Server Error
+
+			res.json({ "error" : err });
+		}
+		else 
+		{
+			// En caso de éxito
+
+			res.status(200);	// OK
+
+			// Retornar los registros de la entidad
+			res.json({ "Challenge" : rows });
+		}
+	});
+
+});
+
+//////////////////////////////////////////////////////////////////
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
